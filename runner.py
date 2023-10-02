a4ac4e1f3885bc6c04c336c93a216164200a2eb7
marshackVB <marshackVB>
2023-10-02 15:23:09 +0000

    initial commit

---
+++ runner.py
@@ -0,0 +1,7 @@
+databricks sync /Users/marshall.carter/Documents/repos/vscode_test /Repos/marshall.carter@databricks.com/vscode_test --watch
+#/Repos/marshall.carter@databricks.com/vscode_test
+def print_function():
+    print("print_function")
+
+def second_print_function():
+    print("second_print")
